d74eb1286c2c3d737b36cc51d2cdcbcb8804e8c6
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-08-29 19:13:30 -0400

    red cycle - mocha test for sum method fails

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+const Calculate = {
+    sum(){
+        
+    }
+};
+
 module.exports = Calculate;
--- test/index_test.js
+++ test/index_test.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
+const Calculate = require('../index.js');
 
 describe('Calculate', () =>{
   describe('.sum', () =>{
      it('returns the sum of an array of numbers', () =>{
-        
+        const expected = 6;
+        const actual = Calculate.sum([1, 2, 3]);
+        assert.strictEqual(actual, expected);
      })
    });
 });
