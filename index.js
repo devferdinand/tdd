0dde8fbddaa2c2398f3317562748598e36c1f0f4
Ferdinand Lukito <devferdinandlukito@gmail.com>
2023-08-29 20:48:13 -0400

    refactored sum method implementation using reduce method

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const Calculate = {
     sum(numArr){
-        let res = 0;
-        numArr.forEach((element) => res += element);
-        return res;
+        return numArr.reduce((accumulator, currentValue) => accumulator + currentValue);
     }
 };
 
